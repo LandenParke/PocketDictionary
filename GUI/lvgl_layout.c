a6fb2c0c093b7407a89210f880b26a87fb524149
AllEarsNazrin <allearsnazrin@gmail.com>
2026-06-16 20:43:48 +0900

    layout and style changes

--- GUI/lvgl_layout.c
+++ GUI/lvgl_layout.c
@@ -1,27 +1,52 @@
 #include "lvgl.h"
 
+// screen size
+int sx = 320;
+int sy = 240;
+
+// horz/vert seperations
+int h1 = 40;
+int h2 = 210;
+int v1 = 40;
+
 
 void create_ui(void)
 {
-
-
 	lv_obj_t *scr = lv_screen_active();
 
-	lv_obj_t *r1 = lv_obj_create(scr);
-	lv_obj_set_pos(r1, 0, 0);
-	lv_obj_set_size(r1, 320, 40);
 
-	lv_obj_t *r2 = lv_obj_create(scr);
-	lv_obj_set_pos(r2, 0, 40);
-	lv_obj_set_size(r2, 60, 200);
+	// top search bar
+	lv_obj_t *c1 = lv_obj_create(scr);
+	lv_obj_set_pos(c1, 0, 0);
+	lv_obj_set_size(c1, sx, h1);
 
-	lv_obj_t *r3 = lv_obj_create(scr);
-	lv_obj_set_pos(r3, 60, 40);
-	lv_obj_set_size(r3, 260, 170);
+	lv_obj_t *c1_label = lv_label_create(c1);
+	lv_label_set_text(c1_label, "search");
 
-	lv_obj_t *r4 = lv_obj_create(scr);
-	lv_obj_set_pos(r4, 60, 210);
-	lv_obj_set_size(r4, 260, 30);
 
+	// left word list
+	lv_obj_t *c2 = lv_obj_create(scr);
+	lv_obj_set_pos(c2, 0, h1);
+	lv_obj_set_size(c2, v1, sy-h1);
+
+
+	// right details panel
+	lv_obj_t *c3 = lv_obj_create(scr);
+	lv_obj_set_pos(c3, v1, h1);
+	lv_obj_set_size(c3, sx-v1, (sy-h1)-(sy-h2));
+
+
+	// bottom options row
+	lv_obj_t *c4 = lv_obj_create(scr);
+	lv_obj_set_pos(c4, v1, h2);
+	lv_obj_set_size(c4, sx-v1, sy-h2);
+
+
+	// gen styling
+	lv_obj_set_style_radius(c1, 0, 0);
+	lv_obj_set_style_radius(c2, 0, 0);
+	lv_obj_set_style_radius(c3, 0, 0);
+	lv_obj_set_style_radius(c4, 0, 0);
+	lv_obj_remove_flag(c1_label, LV_OBJ_FLAG_SCROLLABLE);
 
 }
