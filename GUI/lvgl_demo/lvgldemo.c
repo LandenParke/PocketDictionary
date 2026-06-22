723ab252a3843f9daa935aaf956b4217a7e747db
AllEarsNazrin <allearsnazrin@gmail.com>
2026-06-22 13:56:51 +0900

    lvgl now in 8.3.3 waow

--- GUI/lvgl_demo/lvgldemo.c
+++ GUI/lvgl_demo/lvgldemo.c
@@ -24,29 +24,31 @@
  * Included Files
  ****************************************************************************/
 
-#include <stdio.h>
-#include <nuttx/config.h>
 #include <unistd.h>
+
+
+#include <nuttx/config.h>
 #include <sys/boardctl.h>
-
+#include <sys/param.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <debug.h>
 #include <lvgl/lvgl.h>
-
-
+#include <port/lv_port.h>
+#include <lvgl/demos/lv_demos.h>
 
 
 
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <jp_font_24.c>
+//#include <jp_font_24.c>
 void create_ui(void);
 
 
-
-#include <lvgl/demos/lv_demos.h>
-#ifdef CONFIG_LV_USE_NUTTX_LIBUV
-#include <uv.h>
-#endif
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -56,12 +58,12 @@ void create_ui(void);
  * KEYPAD INPUT
  ****************************************************************************/
 
+
+static lv_indev_drv_t indev_drv;
 static lv_indev_t *kb_indev;
 
-static uint32_t g_last_key;
-static bool g_key_pressed;
-
-#ifndef CONFIG_ARCH_BOARD_SPRESENSE
+static uint32_t last_key;
+static bool key_pressed;
 
 static void set_stdin_nonblock(void)
 {
@@ -81,120 +83,65 @@ static void enable_raw_mode(void)
 
 static void poll_keyboard(void)
 {
-  char c;
-  int n;
+	char c;
+	int n;
 
-  n = read(STDIN_FILENO, &c, 1);
+	n = read(STDIN_FILENO, &c, 1);
 
-  if (n <= 0)
-    {
-      g_key_pressed = false;
-      return;
-    }
+	if (n <= 0) {
+		key_pressed = false;
+		return;
+	}
 
-  switch (c)
-    {
-      case 'w':
-        g_last_key = LV_KEY_UP;
-        g_key_pressed = true;
-        break;
+	switch (c) {
+		case 'w':
+			last_key = LV_KEY_UP;
+			key_pressed = true;
+			break;
 
-      case 's':
-        g_last_key = LV_KEY_DOWN;
-        g_key_pressed = true;
-        break;
+		case 's':
+			last_key = LV_KEY_DOWN;
+			key_pressed = true;
+			break;
 
-      case 'a':
-        g_last_key = LV_KEY_LEFT;
-        g_key_pressed = true;
-        break;
+		case 'a':
+			last_key = LV_KEY_LEFT;
+			key_pressed = true;
+			break;
 
-      case 'r':
-        g_last_key = LV_KEY_RIGHT;
-        g_key_pressed = true;
-        break;
+		case 'r':
+			last_key = LV_KEY_RIGHT;
+			key_pressed = true;
+			break;
 
-      case '\r':
-      case '\n':
-        g_last_key = LV_KEY_ENTER;
-        g_key_pressed = true;
-        break;
+		case '\r':
+		case '\n':
+			last_key = LV_KEY_ENTER;
+			key_pressed = true;
+			break;
 
-      default:
-        g_key_pressed = false;
-        break;
-    }
+		default:
+			key_pressed = false;
+			break;
+	}
 
-  if (g_key_pressed)
-    {
-      printf("LVGL key=%lu\n", (unsigned long)g_last_key);
-      fflush(stdout);
-    }
-}
-
-#else
-
-
-
-// spresense buttons placeholder
-static void poll_buttons(void)
-{
-	if (btn_up_pressed())
-		{
-		g_last_key = LV_KEY_UP;
-		g_key_pressed = true;
-		}
-	else if (btn_down_pressed())
-		{
-		g_last_key = LV_KEY_DOWN;
-		g_key_pressed = true;
-		}
-	else if (btn_left_pressed())
-		{
-		g_last_key = LV_KEY_LEFT;
-		g_key_pressed = true;
-		}
-	else if (btn_right_pressed())
-		{
-		g_last_key = LV_KEY_RIGHT;
-		g_key_pressed = true;
-		}
-	else if (btn_ok_pressed())
-		{
-		g_last_key = LV_KEY_ENTER;
-		g_key_pressed = true;
-		}
-
-	if (g_key_pressed)
-		{
-		printf("LVGL key=%lu\n", (unsigned long)g_last_key);
+	if (key_pressed){
+		printf("LVGL key=%lu\n", (unsigned long)last_key);
 		fflush(stdout);
-		}
+	}
 }
 
-#endif
 
-static void keypad_read_cb(lv_indev_t *indev,
-                           lv_indev_data_t *data) 
-{
-	#ifndef CONFIG_ARCH_BOARD_SPRESENSE
+static void keypad_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
 	poll_keyboard();
-	#else
-	poll_buttons();
-	#endif
 
-	if (g_key_pressed)
-		{
-		data->key = g_last_key;
+	if (key_pressed) {
+		data->key = last_key;
 		data->state = LV_INDEV_STATE_PRESSED;
-		}
-	else
-		{
+	} else {
 		data->state = LV_INDEV_STATE_RELEASED;
-		}
+	}
 }
-
-
 
 
 /****************************************************************************
@@ -205,110 +152,44 @@ static void keypad_read_cb(lv_indev_t *indev,
  * Private Functions
  ****************************************************************************/
 
-#ifdef CONFIG_LV_USE_NUTTX_LIBUV
-static void lv_nuttx_uv_loop(uv_loop_t *loop, lv_nuttx_result_t *result)
-{
-  lv_nuttx_uv_t uv_info;
-  void *data;
-
-  uv_loop_init(loop);
-
-  lv_memset(&uv_info, 0, sizeof(uv_info));
-  uv_info.loop = loop;
-  uv_info.disp = result->disp;
-  uv_info.indev = result->indev;
-#ifdef CONFIG_UINPUT_TOUCH
-  uv_info.uindev = result->utouch_indev;
-#endif
-
-  data = lv_nuttx_uv_init(&uv_info);
-  uv_run(loop, UV_RUN_DEFAULT);
-  lv_nuttx_uv_deinit(&data);
-}
-#endif
-
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
-/****************************************************************************
- * Name: main or lv_demos_main
- *
- * Description:
- *
- * Input Parameters:
- *   Standard argc and argv
- *
- * Returned Value:
- *   Zero on success; a positive, non-zero value on failure.
- *
- ****************************************************************************/
-
 int main(int argc, FAR char *argv[])
 {
-  lv_nuttx_dsc_t info;
-  lv_nuttx_result_t result;
-
-#ifdef CONFIG_LV_USE_NUTTX_LIBUV
-  uv_loop_t ui_loop;
-  lv_memzero(&ui_loop, sizeof(ui_loop));
-#endif
-
-  if (lv_is_initialized())
-    {
-      LV_LOG_ERROR("LVGL already initialized! aborting.");
-      return -1;
-    }
-
-  lv_init();
-
-  lv_nuttx_dsc_init(&info);
-
-#ifdef CONFIG_LV_USE_NUTTX_LCD
-  info.fb_path = "/dev/lcd0";
-#endif
-
-#ifdef CONFIG_INPUT_TOUCHSCREEN
-  info.input_path = CONFIG_EXAMPLES_LVGLDEMO_INPUT_DEVPATH;
-#endif
-
-  lv_nuttx_init(&info, &result);
-
-  if (result.disp == NULL)
-    {
-      LV_LOG_ERROR("lv_demos initialization failure!");
-      return 1;
-    }
 
 
-	// input
-	#ifndef CONFIG_ARCH_BOARD_SPRESENSE
+	boardctl(BOARDIOC_INIT, 0);
+
+	lv_init();
+
+	lv_port_init();
+	
+	// input on pc through terminal for testing
+	lv_indev_drv_init(&indev_drv);	
+	indev_drv.type = LV_INDEV_TYPE_KEYPAD;
+	indev_drv.read_cb = keypad_read;
+	kb_indev = lv_indev_drv_register(&indev_drv);
+	kb_indev = lv_indev_drv_register(&indev_drv);
+	printf("kb_indev=%p\n", kb_indev);
+	fflush(stdout);
 	enable_raw_mode();
 	set_stdin_nonblock();
-	#endif
-	kb_indev = lv_indev_create();
-	lv_indev_set_type(kb_indev, LV_INDEV_TYPE_KEYPAD);
-	lv_indev_set_read_cb(kb_indev, keypad_read_cb);
+
 
 	create_ui();
 
 
-#ifdef CONFIG_LV_USE_NUTTX_LIBUV
-  lv_nuttx_uv_loop(&ui_loop, &result);
-#else
-  while (1)
-    {
-      uint32_t idle;
-      idle = lv_timer_handler();
+	while (1) {
+		uint32_t idle;
+		idle = lv_timer_handler();
 
-      /* Minimum sleep of 1ms */
-
-      idle = idle ? idle : 1;
-      usleep(idle * 1000);
-    }
-#endif
-
-
+		// min sleep 1ms
+		idle = idle ? idle : 1;
+		usleep(idle * 1000);
+	}
+	return 0;
 }
 
 
@@ -495,11 +376,11 @@ static void keyboard_event_cb(lv_event_t *e)
 
 void create_ui(void)
 {
-    lv_obj_t *scr = lv_screen_active();
-    lv_obj_remove_flag(scr, LV_OBJ_FLAG_SCROLLABLE);
-
+    lv_obj_t *scr = lv_scr_act();
+	lv_obj_clear_flag(scr, LV_OBJ_FLAG_SCROLLABLE);
+	
 	// using 24px bitmap from noto sans CJK jp regular
-    lv_obj_set_style_text_font(scr, &jp_font_24, 0);
+    //lv_obj_set_style_text_font(scr, &jp_font_24, 0);
 
 	// root
 	lv_obj_t *root = lv_obj_create(scr);
@@ -566,12 +447,12 @@ void create_ui(void)
 		lv_obj_set_style_pad_all(item, 0, 0);
 		lv_obj_set_style_pad_row(item, 0, 0);
 		lv_obj_set_style_pad_column(item, 0, 0);
-		lv_obj_remove_flag(item, LV_OBJ_FLAG_SCROLLABLE);
+		lv_obj_clear_flag(item, LV_OBJ_FLAG_SCROLLABLE);
 		lv_obj_set_style_radius(item, 0, 0);
 		lv_obj_set_style_pad_all(label, 0, 0);
 		lv_obj_set_style_pad_row(label, 0, 0);
 		lv_obj_set_style_pad_column(label, 0, 0);
-		lv_obj_remove_flag(label, LV_OBJ_FLAG_SCROLLABLE);
+		lv_obj_clear_flag(label, LV_OBJ_FLAG_SCROLLABLE);
 		lv_obj_set_style_radius(label, 0, 0);
 		lv_obj_set_style_border_width(label, 0, 0);
 	}
@@ -617,12 +498,12 @@ void create_ui(void)
 		lv_obj_set_style_pad_all(item, 0, 0);
 		lv_obj_set_style_pad_row(item, 0, 0);
 		lv_obj_set_style_pad_column(item, 0, 0);
-		lv_obj_remove_flag(item, LV_OBJ_FLAG_SCROLLABLE);
+		lv_obj_clear_flag(item, LV_OBJ_FLAG_SCROLLABLE);
 		lv_obj_set_style_radius(item, 0, 0);
 		lv_obj_set_style_pad_all(label, 0, 0);
 		lv_obj_set_style_pad_row(label, 0, 0);
 		lv_obj_set_style_pad_column(label, 0, 0);
-		lv_obj_remove_flag(label, LV_OBJ_FLAG_SCROLLABLE);
+		lv_obj_clear_flag(label, LV_OBJ_FLAG_SCROLLABLE);
 		lv_obj_set_style_radius(label, 0, 0);
 		lv_obj_set_style_border_width(label, 0, 0);
 	}
@@ -654,45 +535,45 @@ void create_ui(void)
 	lv_obj_set_style_pad_all(root, 0, 0);
 	lv_obj_set_style_pad_row(root, 0, 0);
 	lv_obj_set_style_pad_column(root, 0, 0);
-	lv_obj_remove_flag(root, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(root, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(root, 0, 0);
 	// search
 	lv_obj_set_style_pad_all(search, 0, 0);
 	lv_obj_set_style_pad_row(search, 0, 0);
 	lv_obj_set_style_pad_column(search, 0, 0);
-	lv_obj_remove_flag(search, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(search, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(search, 0, 0);
 	// selected word
 	lv_obj_set_style_pad_all(selected, 0, 0);
 	lv_obj_set_style_pad_row(selected, 0, 0);
 	lv_obj_set_style_pad_column(selected, 0, 0);
-	lv_obj_remove_flag(selected, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(selected, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(selected, 0, 0);
 	// middle
 	lv_obj_set_style_pad_all(middle, 0, 0);
 	lv_obj_set_style_pad_row(middle, 0, 0);
 	lv_obj_set_style_pad_column(middle, 0, 0);
-	lv_obj_remove_flag(middle, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(middle, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(middle, 0, 0);
 	lv_obj_set_style_border_width(middle, 0, 0);
 	// word list
 	lv_obj_set_style_pad_all(list, 0, 0);
 	lv_obj_set_style_pad_row(list, 0, 0);
 	lv_obj_set_style_pad_column(list, 0, 0);
-	lv_obj_remove_flag(list, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(list, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(list, 0, 0);
 	lv_obj_set_style_border_width(list, 0, 0);
 	// details
 	lv_obj_set_style_pad_all(details, 0, 0);
 	lv_obj_set_style_pad_row(details, 0, 0);
 	lv_obj_set_style_pad_column(details, 0, 0);
-	lv_obj_remove_flag(details, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(details, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(details, 0, 0);
 	// bottom
 	lv_obj_set_style_pad_all(bottom, 0, 0);
 	lv_obj_set_style_pad_row(bottom, 0, 0);
 	lv_obj_set_style_pad_column(bottom, 0, 0);
-	lv_obj_remove_flag(bottom, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_clear_flag(bottom, LV_OBJ_FLAG_SCROLLABLE);
 	lv_obj_set_style_radius(bottom, 0, 0);
 	lv_obj_set_style_border_width(bottom, 0, 0);
 
