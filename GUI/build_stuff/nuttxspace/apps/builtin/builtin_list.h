d72eabe27805a7f840d4253e99bfcb1a863387f2
AllEarsNazrin <allearsnazrin@gmail.com>
2026-06-22 16:02:05 +0900

    stuff

---
+++ GUI/build_stuff/nuttxspace/apps/builtin/builtin_list.h
@@ -0,0 +1,4 @@
+
+{ "lvgldemo", 100, 32768, lvgldemo_main },
+{ "nsh", 100, 2048, nsh_main },
+{ "sh", 100, 2048, sh_main },
---
+++ GUI/build_stuff/nuttxspace/apps/builtin/builtin_proto.h
@@ -0,0 +1,4 @@
+
+int lvgldemo_main(int argc, char *argv[]);
+int nsh_main(int argc, char *argv[]);
+int sh_main(int argc, char *argv[]);
