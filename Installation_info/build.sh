3763b3fcce0c54e13fdd1ba64ead3a6a23528154
HenryCanapp <henrycanapp@ufl.edu>
2026-07-27 20:22:41 +0900

    Update build.sh
    added a check so you dont have to delete the pocket dictionary folder, and instead it will just manually update
    
    added auto serial input so it just launcches straight into the program

--- Installation_info/build.sh
+++ Installation_info/build.sh
@@ -11,8 +11,24 @@ sudo apt update
 sudo apt install -y $(<packages.txt)
 set -e
 
-echo "Cloning PocketDictionary..."
-git clone "$POCKETDICT_REPO"
+if [ -d "PocketDictionary" ]; then
+    echo "Checking if PocketDictionary is up to date..."
+    cd PocketDictionary
+    git fetch origin
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+    cd ..
+    if [ "$LOCAL" != "$REMOTE" ]; then
+        echo "PocketDictionary is out of date, removing and recloning..."
+        rm -rf PocketDictionary
+        git clone "$POCKETDICT_REPO"
+    else
+        echo "PocketDictionary is up to date."
+    fi
+else
+    echo "Cloning PocketDictionary..."
+    git clone "$POCKETDICT_REPO"
+fi
 
 echo "Entering NuttX directory..."
 cd PocketDictionary/NUTTXSIM/nuttx
@@ -31,4 +47,4 @@ echo "Building..."
 make
 
 echo "Running simulator..."
-./nuttx
+echo (sleep 1; echo "lvgldemo") | ./nuttx
